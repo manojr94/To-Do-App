2c58b4b4bc6de6a9de06793a3a3f3d853f7acd41
Manoj Ravichandran <manojr@freshdesk.com>
2017-01-10 13:45:45 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoApp::Application.config.secret_token = 'c0e306793622f6375ffbd2a3749827722ec947f2ddda1e7e60d04ad81305697ac883994a03232476d5d0b8751c69fcb881262340a59935e86491873ae2030cd6'
